0396e69e3107689536ac0e079e4cdc5e28a74839
somic <onenightatdrliveseys.1988@gmail.com>
2026-03-24 23:59:29 +0200

    Add files via upload

---
+++ music.js
@@ -0,0 +1,6 @@
+// music.js
+const musicPlaylist = [
+    "https://www.dropbox.com/scl/fi/d9oxw4c1gh9jvf3g4ol7g/Mii-Editor-Mii-Maker-Wii-U-Music.mp3?rlkey=02gozi20og6da5zi979e8gh9t&st=zria9uzy&dl=1",
+    "https://www.tonyciccarone.com/uploads/1/3/4/6/134633856/frutiger_aero_music_sample.mp3", // Пример другого трека
+    // Добавляй свои ссылки сюда
+];
