f21fda23215e553438ab7a3c51c9a0068b32a88e
somic <onenightatdrliveseys.1988@gmail.com>
2026-03-25 00:41:26 +0200

    Replace music track URL in musicPlaylist

--- music.js
+++ music.js
@@ -2,7 +2,7 @@
 // Добавляй треки как обычные строки (как слова в словаре).
 const musicPlaylist = [
     "https://www.dropbox.com/scl/fi/d9oxw4c1gh9jvf3g4ol7g/Mii-Editor-Mii-Maker-Wii-U-Music.mp3?rlkey=02gozi20og6da5zi979e8gh9t&st=zria9uzy&dl=1",
-    "https://www.tonyciccarone.com/uploads/1/3/4/6/134633856/frutiger_aero_music_sample.mp3"
+    "https://www.dropbox.com/scl/fi/quuraep8dko7n3o4kbehc/AIRPORT-SONG-New-Retro.mp3?rlkey=hao0nf3u46h53880om02h7yab&st=u284toru&dl=1"
 ];
 
 // Можно вызывать из консоли: addMusicTrack("https://site.com/track.mp3")
